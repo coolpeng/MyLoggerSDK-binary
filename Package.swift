1c65ce7979026b0936ec0225d1dc85a60c64e0b0
penglj3 <penglj3@lenovo.com>
2026-02-11 15:30:01 +0800

    Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyLoggerSDK",
-            url: "https://github.com/coolpeng/MyLoggerSDK-binary/releases/download/1.0.0/MyLoggerSDK-1.0.0.xcframework.zip",
+            url: "https://github.com/coolpeng/MyLoggerSDK-binary/releases/download/1.0.1/MyLoggerSDK-1.0.1.xcframework.zip",
             checksum: "c42554af7e29234b48ff22ad3b021a500555f912aca815d48da994772bd02f11"
         )
     ]
